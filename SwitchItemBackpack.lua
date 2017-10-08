020deed01f6f192aca80983a80a0aed14eb6b907
Rhythm-PC\Rhythm <mlambers@mail.ccsf.edu>
2017-10-09 02:48:32 +0800

    Change logic behind
    version 0.2

--- SwitchItemBackpack.lua
+++ SwitchItemBackpack.lua
@@ -1,4 +1,4 @@
--- Version: [0.1] - 08-10-2017
+-- Version: [0.2] - 08-10-2017
 
 local SwitchItemBackpack = {}
 
@@ -6,107 +6,86 @@ SwitchItemBackpack.Enable = Menu.AddOption({ "Utility", "One Key Item to Backpac
 SwitchItemBackpack.optionKey = Menu.AddKeyOption({"Utility", "One Key Item to Backpack"}, "Key for activate", Enum.ButtonCode.KEY_D)
 
 SwitchItemBackpack.itemList = {
-	item_ward_dispenser = true,
-	item_tango_single = true,
+	item_branches = true,
+	item_gauntlets = true,
+	item_mantle = true,
+	item_circlet = true,
+	item_belt_of_strength = true,
+	item_robe = true,
+	item_ogre_axe = true,
+	item_staff_of_wizardry = true,
 	
-	item_power_treads = true,
-	
-	item_recipe_hand_of_midas = true,
-	item_recipe_oblivion_staff = true,
-	item_recipe_wraith_band = true,
-	item_recipe_null_talisman = true,
-	item_recipe_mekansm = true,
-	
-	item_clarity = true,
-	item_faerie_fire = true,
-	item_enchanted_mango = true,
-	item_tango = true,
-	item_flask = true,
-	item_smoke_of_deceit = true,
-	item_tpscroll = true,
-	item_dust = true,
-	item_courier = true,
-	item_flying_courier = true,
-	item_ward_observer = true,
-	item_ward_sentry = true,
-	item_tome_of_knowledge = true,
-	item_bottle = true,
-	
-	item_slippers = true,
-	item_boots_of_elves = true,
-	item_blade_of_alacrity = true,
-	
-	item_ring_of_protection = true,
-	item_stout_shield = true,
-	item_quelling_blade = true,
-	item_infused_raindrop = true,
-	item_blight_stone = true,
-	item_orb_of_venom = true,
-	item_blades_of_attack = true,
-	item_chainmail = true,
-	item_quarterstaff = true,
-	item_helm_of_iron_will = true,
-	item_broadsword = true,
-	item_claymore = true,
-	item_javelin = true,
-	item_mithril_hammer = true,
-	
-	item_wind_lace = true,
-	item_magic_stick = true,
-	item_sobi_mask = true,
-	item_ring_of_regen = true,
-	item_boots = true,
-	item_gloves = true,
-	item_cloak = true,
-	item_ring_of_health = true,
-	item_void_stone = true,
-	item_gem = true,
-	item_lifesteal = true,
-	item_shadow_amulet = true,
 	item_ghost = true,
-	item_blink = true,
 	
-	item_platemail = true,
-	item_talisman_of_evasion = true,
-	item_hyperstone = true,
-	item_demon_edge = true,
-	item_eagle = true,
-	item_relic = true,
+	item_energy_booster = true,
+	item_vitality_booster = true,
+	item_point_booster = true,
+	item_ultimate_orb = true,
+	item_mystic_staff = true,
+	item_reaver = true,
 	
-	item_poor_mans_shield = true,
-	item_soul_ring = true,
-	item_phase_boots = true,
-	item_pers = true,
-	item_hand_of_midas = true,
-	item_travel_boots = true,
-	item_moon_shard = true,
+	item_magic_wand = true,
+	item_null_talisman = true,
+	item_wraith_band = true,
+	item_bracer = true,
+	item_oblivion_staff = true,
 	
-	item_ring_of_basilius = true,
-	item_iron_talon = true,
-	item_tranquil_boots = true,
-	item_medallion_of_courage = true,
-	item_pipe = true,
+	item_headdress = true,
+	item_buckler = true,
+	item_urn_of_shadows = true,
+	item_ring_of_aquila = true,
+	item_arcane_boots = true,
+	item_ancient_janggo = true,
+	item_mekansm = true,
+	item_vladmir = true,
+	item_guardian_greaves = true,
 	
-	item_glimmer_cape = true,
-	item_solar_crest = true,
+	item_force_staff = true,
+	item_veil_of_discord = true,
+	item_aether_lens = true,
+	item_necronomicon = true,
+	item_necronomicon_2 = true,
+	item_necronomicon_3 = true,
+	item_dagon = true,
+	item_dagon_2 = true,
+	item_dagon_3 = true,
+	item_dagon_4 = true,
+	item_dagon_5 = true,
+	item_cyclone = true,
+	item_rod_of_atos = true,
+	item_orchid = true,
+	item_ultimate_scepter = true,
+	item_sheepstick = true,
+	item_octarine_core = true,
 	
-	item_hood_of_defiance = true,
-	item_assault = true,
+	item_vanguard = true,
+	item_blade_mail = true,
+	item_soul_booster = true,
+	item_crimson_guard = true,
+	item_black_king_bar = true,
+	item_lotus_orb = true, 
+	item_shivas_guard = true,
+	item_manta = true,
+	item_sphere = true,
+	item_hurricane_pike = true,
+	item_heart = true,
 	
-	item_lesser_crit = true,
-	item_armlet = true,
-	item_invis_sword = true,
-	item_bfury = true,
-	item_radiance = true,
-	item_monkey_king_bar = true,
-	item_butterfly = true,
-	item_rapier = true,
+	item_basher = true,
+	item_ethereal_blade = true,
+	item_silver_edge = true,
+	item_abyssal_blade = true,
+	item_bloodthorn = true,
 	
-	item_mask_of_madness = true,
-	item_yasha = true,
-	item_maelstrom = true,
-	item_desolator = true,
-	item_mjollnir = true
+	item_helm_of_the_dominator = true,
+	item_dragon_lance = true,
+	item_sange = true,
+	item_echo_sabre = true,
+	item_diffusal_blade = true,
+	item_diffusal_blade_2 = true,
+	item_heavens_halberd = true,
+	item_sange_and_yasha = true,
+	item_skadi = true,
+	item_satanic = true
 }
 SwitchItemBackpack.sleepers = {}
 
@@ -120,7 +99,7 @@ function SwitchItemBackpack.OnUpdate()
 	if not myHero then
 		return
 	end
-
+	
 	if Menu.IsKeyDown(SwitchItemBackpack.optionKey) then
 		SwitchItemBackpack.backpackItem(myHero, 0)
 		SwitchItemBackpack.backpackItem(myHero, 1)
@@ -136,7 +115,7 @@ function SwitchItemBackpack.backpackItem(heroEnt, idxItem)
 	if  itemEnt ~= nil and Entity.IsAbility(itemEnt) then 
 		local itemName = Ability.GetName(itemEnt)
 				
-		if not SwitchItemBackpack.itemList[itemName] and SwitchItemBackpack.SleepCheck(1, "backpack" .. idxItem) then
+		if 	SwitchItemBackpack.itemList[itemName] and SwitchItemBackpack.SleepCheck(1, "backpack" .. idxItem) then
 			SwitchItemBackpack.MoveItemToSlot(heroEnt, itemEnt, math.random (6, 8))
 			SwitchItemBackpack.MoveItemToSlot(heroEnt, itemEnt, idxItem)
 			SwitchItemBackpack.Sleep(1, "backpack" .. idxItem)
